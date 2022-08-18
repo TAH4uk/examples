6a4fad8d9f0df159d8aff158dbb53df1c584da37
TAH4uk <pcs_zero@inbox.ru>
2022-08-18 19:34:05 +0300

    Enamp05

---
+++ Examples/Examp05/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+
+Console.WriteLine("Ура ЭТО МАШКА");
+
+} 
+else
+{
+
+Console.Write("Привет ");
+Console.WriteLine(username);
+
+}
