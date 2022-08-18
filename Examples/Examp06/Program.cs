676327601ae31f04a2c38eae790c0f47368505b0
TAH4uk <pcs_zero@inbox.ru>
2022-08-18 19:40:50 +0300

    Examp06

--- Examples/Examp06/Program.cs
+++ Examples/Examp06/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 3;
+int c = 8;
+int d = 6;
+int e = 5;
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine (max);
